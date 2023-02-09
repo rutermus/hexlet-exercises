f9af15d453d1edcad6066c66fa45422ba5556879
termus <rutermus@gmail.com>
2023-02-09 13:49:34 +0300

    add l8

---
+++ frontend/js-basics/l8-variables-expression/exercise.js
@@ -0,0 +1,12 @@
+const eurosCount = 100;
+
+// BEGIN (write your solution here)
+const eurPerUsd = 1.25;
+const usdPerRub = 60;
+
+const usdCount = eurosCount * eurPerUsd;
+console.log(usdCount);
+
+const rubCount = usdCount * usdPerRub;
+console.log(rubCount);
+// END
